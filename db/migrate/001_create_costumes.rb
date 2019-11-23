71ee849e81df5fcd6e60e18355e635d8f29d62e3
Vanna Winland <vgwinland@gmail.com>
2019-11-23 08:42:06 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -4,6 +4,10 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
       create_table :costumes do |t|
         t.string :name
+        t.string :price
+        t.string :size
+        t.string :imageurl
+        
 
         t.timestamps null: false
 
