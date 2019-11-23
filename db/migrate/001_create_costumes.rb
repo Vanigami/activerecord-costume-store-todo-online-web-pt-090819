eb51e42f6008dd1a24c4c97a169540725e0506cb
Vanna Winland <vgwinland@gmail.com>
2019-11-23 08:47:04 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,7 +5,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
       create_table :costumes do |t|
         t.string :name
         t.string :price
-        t.intger :size
+        t.integer :size
         t.string :imageurl
 
 
