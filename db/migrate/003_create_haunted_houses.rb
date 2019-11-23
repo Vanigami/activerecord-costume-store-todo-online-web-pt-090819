fcc6a59746d23af09d5ae670b24d5be60fe77293
Vanna Winland <vgwinland@gmail.com>
2019-11-23 09:00:55 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,9 +6,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :theme
       t.integer :price
       t.boolean :family_friendly
-      t.datetime :opening_time
-      t.datetime :closing_time
-      t.integer :description
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :description
 
       t.timestamps
     end
